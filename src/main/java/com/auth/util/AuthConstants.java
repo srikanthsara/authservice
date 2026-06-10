73786a7c670233e6021814684c5dd4555da8e1ff
Srikanth Sara <srikanth.wankar@gmail.com>
2026-06-10 15:07:26 +0100

    Added checkstyle,sonar scan and jacoco report configuration

---
+++ src/main/java/com/auth/util/AuthConstants.java
@@ -0,0 +1,7 @@
+package com.auth.util;
+
+public class AuthConstants {
+    public static final String SECRET =
+            "ksana-grocery-authentication-jwt-secret-key-2026";
+    public static final String DAY_TIME_IN_SEC = "864000";
+}
